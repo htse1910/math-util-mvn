e23afb514ebaa90e446b4249e184911a998e7046
htse1910 <hoangtrieuduong1910@gmail.com>
2023-06-26 09:07:38 +0700

    update getf() to use recursion

--- src/main/java/org/lucas/mathutil/core/MathUtility.java
+++ src/main/java/org/lucas/mathutil/core/MathUtility.java
@@ -16,16 +16,27 @@ public class MathUtility {
     //21 giai thừa là vượt kiểu long, vượt 18 con số 0
     //mình ko chấp nhận n > 20
     // gài ràng buộc đầu vào cho hàm/method
-    public static final double PI = 3.1415;
+//    public static final double PI = 3.1415;
+//        public static long getFactorial(int n){
+//            long product = 1; // tích ban đầu = 1
+//            if( n < 0 ||n>20)
+//                throw new IllegalArgumentException("Invalid n! n must be between 0 to 20, please");
+//            if (n == 0 || n == 1 )
+//                return 1;
+//            for(int i = 2; i<=n; i++)
+//                product *=i;
+//            return product;
+//        }
+//        //return sớm để hàm ko báo lỗi
+//    
+     public static final double PI = 3.1415;
         public static long getFactorial(int n){
-            long product = 1; // tích ban đầu = 1
             if( n < 0 ||n>20)
                 throw new IllegalArgumentException("Invalid n! n must be between 0 to 20, please");
             if (n == 0 || n == 1 )
                 return 1;
-            for(int i = 2; i<=n; i++)
-                product *=i;
-            return product;
+            
+            return n * getFactorial(n - 1);
         }
         //return sớm để hàm ko báo lỗi
     
